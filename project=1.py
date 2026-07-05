349879acc2604d2925947b2c037e2ac5261ec8a1
Hasan <hasanamagov5@gmail.com>
2026-07-05 22:09:02 +0100

    add my project

---
+++ project=1.py
@@ -0,0 +1,32 @@
+
+def calculator(a,b,op):
+    try:
+        if op == "+":
+            return a + b 
+        if op == "-":
+            return a - b 
+        if op == "*":
+            return a * b
+        if op == "/":
+            if b == 0:
+                return a / b
+    except ValueError:
+        print("error you most write number not 0")
+
+try:
+    a = float(input("write number one"))
+    
+except:
+    print("online number")
+try:
+
+    b = float(input("write number two"))
+except:
+    print("online number 2")
+try:
+    op = input("write skil! + , - , * , / ")
+except ValueError:
+    print("+ - * /")
+
+s = calculator(a,b,op)
+print(s)
